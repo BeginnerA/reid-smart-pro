5c0bf8150a05dd575071584609b0fb501b4ab4e4
REID <1184010001@qq.com>
2022-01-05 10:35:29 +0800

    ADD: 字符串处理工具

---
+++ reid-smart-base/src/main/java/top/reid/smart/core/util/CommonCharacter.java
@@ -0,0 +1,19 @@
+package top.reid.smart.core.util;
+
+/**
+ * <p>
+ * 常用字符
+ * </p>
+ *
+ * @Author REID
+ * @Blog https://blog.csdn.net/qq_39035773
+ * @GitHub https://github.com/BeginnerA
+ * @Data 2021/12/31
+ * @Version V1.0
+ **/
+public interface CommonCharacter {
+    /**
+     * 逗号（英文）
+     */
+    String COMMA = ",";
+}
