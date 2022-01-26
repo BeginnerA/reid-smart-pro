348ea99226ff5e20c72d69274199c1abb46a26ff
REID <1184010001@qq.com>
2022-01-26 16:14:56 +0800

    ADD: 整合 MyBatis-Plus

---
+++ reid-smart-base/src/main/java/top/reid/config/mybatis/MybatisPlusConfig.java
@@ -0,0 +1,35 @@
+package top.reid.config.mybatis;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import top.reid.smart.spring.annotation.MapperScan;
+
+/**
+ * <p>
+ * MyBatis-Plus 配置类
+ * </p>
+ *
+ * @Author REID
+ * @Blog https://blog.csdn.net/qq_39035773
+ * @GitHub https://github.com/BeginnerA
+ * @Data 2021/11/19
+ * @Version V1.0
+ **/
+@MapperScan("${reid.mybatis.mapperScanner.basePackage}")
+@Configuration
+public class MybatisPlusConfig {
+    /**
+     * 新的分页插件，一缓和二缓遵循 mybatis 的规则，<br>
+     * 需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+
+}
