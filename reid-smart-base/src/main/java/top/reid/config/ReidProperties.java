629f6e174fbfb977b1e1f0efff86761ba52fe789
REID <1184010001@qq.com>
2022-03-09 09:13:30 +0800

    FIX：优化配置项

---
+++ reid-smart-base/src/main/java/top/reid/config/ReidProperties.java
@@ -0,0 +1,37 @@
+package top.reid.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import top.reid.config.mybatis.MybatisProperties;
+import top.reid.config.shiro.ShiroProperties;
+import top.reid.config.swagger.SwaggerProperties;
+
+/**
+ * <p>
+ * 配置项
+ * </p>
+ *
+ * @Author REID
+ * @Blog https://blog.csdn.net/qq_39035773
+ * @GitHub https://github.com/BeginnerA
+ * @Data 2022/3/9
+ * @Version V1.0
+ **/
+@Data
+@Component
+@ConfigurationProperties(prefix = "reid")
+public class ReidProperties {
+    /**
+     * Mybatis 配置项
+     */
+    MybatisProperties mybatis;
+    /**
+     * Shiro 配置项
+     */
+    ShiroProperties shiro;
+    /**
+     * Swagger 配置项
+     */
+    SwaggerProperties swagger;
+}
