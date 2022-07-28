a4d3acab113f1a2b33c14fc464ebf8a844f58f4c
Sissi <1184010001@qq.com>
2022-07-28 14:54:41 +0800

    ADD:物联网模块（完成消息订阅、消息推送、取消订阅、消息持久化）

---
+++ reid-smart-system/src/main/java/org/reid/smart/modules/iot/IotController.java
@@ -0,0 +1,74 @@
+package org.reid.smart.modules.iot;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.reid.common.api.vo.Result;
+import top.reid.iot.entity.Publish;
+import top.reid.iot.entity.Subscribe;
+import top.reid.iot.service.mqtt.PublisherService;
+import top.reid.iot.service.mqtt.SubscribeService;
+import top.reid.iot.service.mqtt.UnsubscribeService;
+
+import javax.annotation.Resource;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @Author REID
+ * @Blog <a href="https://blog.csdn.net/qq_39035773">Blog</a>
+ * @GitHub <a href="https://github.com/BeginnerA">GitHub</a>
+ * @Data 2022/7/27
+ * @Version V1.0
+ **/
+@Api(tags = "MQTT 测试")
+@RestController
+@RequestMapping("/iot")
+public class IotController {
+
+    @Resource
+    SubscribeService subscribeService;
+    @Resource
+    PublisherService publisherService;
+    @Resource
+    UnsubscribeService unsubscribeService;
+
+    @PostMapping("/subscribeToInform")
+    @ApiOperation(value = "订阅消息-同步")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "topic", value = "订阅主题", dataType = "String", required = true),
+    })
+    public Result<?> subscribeToInform(String topic) {
+        Subscribe subscribe = new Subscribe();
+        subscribe.setTopic(topic);
+        return Result.ok(subscribeService.subscribe(subscribe));
+    }
+
+    @PostMapping("/publishInform")
+    @ApiOperation(value = "推送消息-同步")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "message", value = "推送的消息报文", dataType = "String", required = true),
+            @ApiImplicitParam(name = "topic", value = "订阅主题", dataType = "String", required = true),
+    })
+    public Result<?> publishInform(String message, String  topic) {
+        Publish publish = new Publish();
+        publish.setTopic(topic);
+        publish.setMessage(message);
+        return Result.ok(publisherService.publish(publish));
+    }
+
+    @PostMapping("/unsubscribeInform")
+    @ApiOperation(value = "取消订阅-同步")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "topic", value = "订阅主题", dataType = "String", required = true),
+    })
+    public Result<?> unsubscribeInform(String  topic) {
+        return Result.ok(unsubscribeService.unsubscribe(topic));
+    }
+}
